f9aedef8730e9d10ce8d9b10d4313cf1bbd46ceb
Jioh19 <jioh1982@gmail.com>
2023-05-24 11:26:11 -0400

    Etrega final

--- M5S7 REBOUND.sql
+++ M5S7 REBOUND.sql
@@ -11,10 +11,15 @@ SELECT folio, fecha, monto, nombre, cliente.rut
     FROM venta LEFT JOIN cliente ON venta.rut = cliente.rut;
 
 -- 3. Clasificar los clientes según la siguiente tabla:
-SELECT cliente.rut, cliente.nombre, SUM(monto), EXTRACT(YEAR FROM fecha) as Año
-    FROM cliente INNER JOIN venta ON venta.rut = cliente.rut 
-        GROUP BY  cliente.rut, EXTRACT(YEAR FROM fecha)
-			ORDER BY año;
+SELECT cliente.rut, cliente.nombre, SUM(monto) as total, EXTRACT(YEAR FROM fecha) as Año, 
+	CASE
+		WHEN SUM(monto) <= 1000000 THEN 'Standar'
+		WHEN SUM(monto) <= 3000000 THEN 'Gold'
+		ELSE 'Premium'
+	END AS casificacion
+        FROM cliente INNER JOIN venta ON venta.rut = cliente.rut 
+            GROUP BY  cliente.rut, EXTRACT(YEAR FROM fecha)
+                ORDER BY año;
 
 -- 4. Por medio de una subconsulta, listar las ventas con la marca del vehículo más vendido.
 SELECT s.marca AS "Más vendido", COUNT(*) AS "Cantidad", SUM(monto) AS "Total" 
